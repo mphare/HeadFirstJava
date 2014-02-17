9923a86d02055e6d017668f044b772d11d131a2d
Michael Hare <mphare@whitehare.com>
2014-02-16 19:22:37 -0600

    Working DOtCom v2

--- DotCom_v2/src/DotCom.java
+++ DotCom_v2/src/DotCom.java
@@ -19,4 +19,27 @@ public class DotCom
   {
     locationCells = loc;
   }
+
+  public String checkYourself(String userInput)
+  {
+    String result = "miss";
+
+    int index = locationCells.indexOf(userInput);
+
+    if (index >= 0)
+    {
+      locationCells.remove(index);
+
+      if (locationCells.isEmpty())
+      {
+        result = "kill";
+        System.out.println("Ouch! You sunk " + name + " : (");
+      } else
+      {
+        result = "hit";
+      }
+    }
+
+    return result;
+  }
 }
--- DotCom_v2/src/GameHelper.java
+++ DotCom_v2/src/GameHelper.java
@@ -1,13 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class GameHelper
 {
+  private static final String alphabet   = "abcdefg";
+  private int                 gridLength = 7;
+  private int                 gridSize   = 49;
+  private int[]               grid       = new int[gridSize];
+  private int                 comCount   = 0;
+
   public String getUserInput(String prompt)
   {
     String inputLine = null;
-    System.out.println(prompt + " ");
+    System.out.print(prompt + " ");
     try
     {
       BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
@@ -19,6 +26,62 @@ public class GameHelper
     {
       System.out.println("IOException: " + e);
     }
-    return inputLine;
+    return inputLine.toLowerCase();
+  }
+
+  public ArrayList<String> placeDotCom(int comSize)
+  {
+    ArrayList<String> alphaCells = new ArrayList<String>();
+
+    String temp = null;
+    int[] coords = new int[comSize];
+    int attempts = 0;
+    boolean success = false;
+    int location = 0;
+    comCount++;
+    int incr = 1;
+    if ((comCount % 2) == 1)
+    {
+      incr = gridLength;
+    }
+
+    while (!success & attempts++ < 200)
+    {
+      location = (int) (Math.random() * gridSize);
+      int x = 0;
+      success = true;
+      while (success && x < comSize)
+      {
+        if (grid[location] == 0)
+        {
+          coords[x++] = location;
+          location += incr;
+          if (location >= gridSize)
+          {
+            success = false;
+          }
+          if (x > 0 && (location % gridLength == 0))
+          {
+            success = false;
+          }
+        } else
+        {
+          success = false;
+        }
+      }
+    }
+    int x = 0;
+    int row = 0;
+    int column = 0;
+    while (x < comSize)
+    {
+      grid[coords[x]] = 1;
+      row = (int) (coords[x] / gridLength);
+      column = coords[x] % gridLength;
+      temp = String.valueOf(alphabet.charAt(column));
+      alphaCells.add(temp.concat(Integer.toString(row)));
+      x++;
+    }
+    return alphaCells;
   }
 }
