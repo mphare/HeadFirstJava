6d63763a79b0089ba35357b71d1a4b59754e7c2c
Michael Hare <mphare@whitehare.com>
2014-02-26 21:23:48 -0600

    Second version of Midi Event Controller

--- MidiEvent-v2/src/MiniMusicPlayer2.java
+++ MidiEvent-v2/src/MiniMusicPlayer2.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.ControllerEventListener;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
@@ -5,47 +6,62 @@ import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
-public class MiniMusicPlayer1
+public class MiniMusicPlayer2 implements ControllerEventListener
 {
 
   public static void main(String[] args)
+  {
+    MiniMusicPlayer2 mini = new MiniMusicPlayer2();
+    mini.go();
+
+  }
+
+  public void go()
   {
     try
     {
       Sequencer sequencer = MidiSystem.getSequencer();
       sequencer.open();
+      int[] eventsIWant = { 127 };
+      sequencer.addControllerEventListener(this, eventsIWant);
 
       Sequence seq = new Sequence(Sequence.PPQ, 4);
       Track track = seq.createTrack();
-
       for (int i = 5; i < 61; i += 4)
       {
         track.add(makeEvent(144, 1, i, 100, i));
+        track.add(makeEvent(176, 1, 127, 0, i));
         track.add(makeEvent(128, 1, i, 100, i + 2));
       }
+
       sequencer.setSequence(seq);
       sequencer.setTempoInBPM(220);
       sequencer.start();
+
     } catch (Exception ex)
     {
       ex.printStackTrace();
     }
-
   }
 
-  public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick)
+  public void controlChange(ShortMessage event)
+  {
+    System.out.println("la");
+  }
+
+  public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick)
   {
     MidiEvent event = null;
     try
     {
-      ShortMessage s = new ShortMessage();
-      s.setMessage(comd, chan, one, two);
-      event = new MidiEvent(s, tick);
-
+      ShortMessage a = new ShortMessage();
+      a.setMessage(comd, chan, one, two);
+      event = new MidiEvent(a, tick);
     } catch (Exception ex)
     {
       ex.printStackTrace();
     }
     return event;
   }
+
 }
