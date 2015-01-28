08c3805deb73f9e52fe9eafee779774bb18229ea
Michael Hare <mhare@zixcorp.com>
2015-01-28 15:55:08 -0600

    Updated

--- TwoButtons/src/MyDrawPanel.java
+++ TwoButtons/src/MyDrawPanel.java
@@ -5,8 +5,10 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class MyDrawPanel extends JPanel
 {
+
   public void paintComponent(Graphics g)
   {
     Graphics2D g2d = (Graphics2D) g;
